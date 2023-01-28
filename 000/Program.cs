844e10a4d65a05db9fe1341588fe48d28119b890
KseniaSvetlova <svetlovakd@icloud.com>
2023-01-28 17:30:22 +0400

    Lesson1 01

---
+++ 000/Program.cs
@@ -0,0 +1,21 @@
+﻿// С клавиатуры вводятся два числа. Найти и выывести на экран их сумму.
+//string s;
+int a,b,sum;
+a=Convert.ToInt32(Console.ReadLine());
+b=Convert.ToInt32(Console.ReadLine());
+sum=a+b;
+//1. Конкатинация (склеивание)строк.
+System.Console.WriteLine(a+"+"+b.ToString()+"="+Convert.ToString(sum));
+//2. Форматированный вывод.
+System.Console.WriteLine("{0:F2}+{1:E5}={2:G4}",a,b,sum);
+//3. Интерполяция строк.
+System.Console.WriteLine($"{a:F2}+{b:E5}={sum:G4}");
+
+// С клавиатуры вводятся два числа. Найти и выывести на экран максимальное з них.
+
+int max;
+if(a>b)
+    max=a;
+else
+    max=b;
+    System.Console.WriteLine($"max={max}");
---
+++ 001/Program.cs
@@ -0,0 +1,6 @@
+﻿// 1. С клавиатуры вводится число. Вывести квадрат числа
+string? s;
+int a;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+Console.WriteLine(Math.Pow(a,2));
